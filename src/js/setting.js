18cc91cb8b62e2c8761422a5dcd9de5fd55d83c9
下北泽工作室 <119436353+xbzstudio@users.noreply.github.com>
2023-05-03 20:46:31 +0800

    添加可更改设置（2个）

--- src/js/setting.js
+++ src/js/setting.js
@@ -3,6 +3,8 @@ var setting = {
     autoTranslate:true, //是否默认启用自动翻译，true为是，false为否
     tokenToServer:true, //是否默认启用连续对话，true为是，false为否
     chatStyle:"creative", //与Bing AI聊天时选用的聊天风格，balanced代表平衡，creative代表创造力，precise代表精确性
-    tips:"" //在新的主题开始时的第一条对话前插入的提示，null和空的英文双引号（""）表示没有。
+    tips:"", //在新的主题开始时的第一条对话前插入的提示，null和空的英文双引号（""）表示没有。
+    fontColor:"white", //你和Bing AI在消息框中的消息字体颜色
+    nameColor:"#dadada", //你和Bing AI显示名字的颜色
 
 };
