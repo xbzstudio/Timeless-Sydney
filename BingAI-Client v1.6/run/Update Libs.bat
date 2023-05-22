a0d3ffabba02ed934293c55053e49a376c65751c
下北泽工作室 <119436353+xbzstudio@users.noreply.github.com>
2023-05-22 22:14:21 +0800

    1.6更新！

---
+++ BingAI-Client v1.6/run/Update Libs.bat
@@ -0,0 +1,5 @@
+pip install --upgrade EdgeGPT -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install --upgrade fastapi -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install --upgrade BingImageCreator -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install --upgrade uvicorn -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install --upgrade python-multipart -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
