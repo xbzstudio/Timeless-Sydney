2d701acb404d60f5e5dc8ed17f465256a6b8bafc
下北泽工作室 <119436353+xbzstudio@users.noreply.github.com>
2023-05-25 22:36:40 +0800

    Update Update Libs.bat

--- run/Update Libs.bat
+++ run/Update Libs.bat
@@ -1,4 +1,4 @@
-pip install --upgrade EdgeGPT -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install --upgrade EdgeGPT==0.4.4 -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install --upgrade fastapi -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install --upgrade BingImageCreator -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install --upgrade uvicorn -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
