268a729d94b209922827c70e836d648b4a25fd82
下北泽工作室 <119436353+xbzstudio@users.noreply.github.com>
2023-06-01 22:37:10 +0800

    Add files via upload

---
+++ run/Install Libs.bat
@@ -0,0 +1,7 @@
+pip install EdgeGPT==0.6.10 -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install BingImageCreator -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install fastapi -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install uvicorn -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install python-multipart -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install pyqt5 -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
+pip install PyQtWebEngine -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
