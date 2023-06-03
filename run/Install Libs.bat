9283f212a63872737f78f02b1e1ccf0e5e2ae142
下北泽工作室 <119436353+xbzstudio@users.noreply.github.com>
2023-06-03 11:17:39 +0800

    Update Install Libs.bat

--- run/Install Libs.bat
+++ run/Install Libs.bat
@@ -1,7 +1,6 @@
+pip install aiohttp -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install EdgeGPT==0.6.10
 pip install BingImageCreator -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install fastapi -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install uvicorn -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
 pip install python-multipart -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-pip install pyqt5 -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
-pip install PyQtWebEngine -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
