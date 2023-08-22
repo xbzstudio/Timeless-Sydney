6a86c91e285f36b0ab7fb7b333090b23065d3683
SUNWUYUAN <1847261658@qq.com>
2023-08-22 11:15:07 +0800

    1

--- config/client.js
+++ config/client.js
@@ -2,7 +2,7 @@
 var settings = {
     "backgroundUrl" : "./static/Background.jpg",
     "connectTo" : 'http://127.0.0.1',
-    "port" : 80,
+    "port" : 20808,
     "tipName" : 'Sydney',
     'style' : 'creative',
     'cookie' : '',
